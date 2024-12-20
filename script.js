d384bc06da99a0144e4d55c04370f37857d6833d
Mzmff333 <mzmff55533@gmail.com>
2024-12-20 21:57:09 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+let name = prompt('Enter your name:');
+console.log(name);  // This will log the name inputted by the user
+
+let name2 = prompt('Enter another name:');
+console.log('Hello ' + name + ' ' + name2);  // This will print a greeting with both names
